5b1a087104fb2ae00f99be361f7242da0550e7f6
Lucas Bustos <lbustos.silva@icloud.com>
2023-04-11 13:18:59 -0300

    Rota de teste

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,4 +7,13 @@ const routes = Router();
 routes.use('/torneios', tournamentRouter);
 routes.use('/usuarios', userRouter);
 
+routes.get('/', async (req, res) => {
+  try {
+    return res.status(200).json({ message: 'OK' });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
+});
+
 export default routes;
